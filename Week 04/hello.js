0c10ba77d95d31ebba3ffab9df0732ba147a496c
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-02-12 15:38:08 +0530

    js factories

---
+++ Week 04/hello.js
@@ -0,0 +1,15 @@
+function printChai() {
+  console.log("Chai peelo guys...");
+}
+
+function bringBrush(itneBrush) {
+  console.log(`Haanji, lee aye ${itneBrush} brush.`);
+}
+
+function sumOfTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+printChai();
+bringBrush(5);
+console.log(sumOfTwoNumbers(2, 4));
