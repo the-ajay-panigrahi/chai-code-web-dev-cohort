c48bfa0befafd4b58fccee2f06cb260db219201b
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-02-13 15:42:39 +0530

    conditionals

---
+++ Week 04/conditionals.js
@@ -0,0 +1,23 @@
+const firstName = "Ajay";
+if (firstName === "Ajay") {
+  console.log("Chai & Code");
+} else {
+  console.log("Time Pass Karooo...");
+}
+
+let num = 10;
+if (num % 2 === 0) {
+  console.log(`${num} is EVEN!`);
+} else {
+  console.log(`${num} is ODD!`);
+}
+
+// Check whether the number is Positive, Negative or Zero
+num = 0;
+if (num > 0) {
+  console.log(`${num} is POSITIVE!`);
+} else if (num < 0) {
+  console.log(`${num} is NEGATIVE!`);
+} else {
+  console.log(`${num} is ZERO!`);
+}
