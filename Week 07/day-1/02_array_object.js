dc990f592e415084d7b3ebca0cb18051f4cf02b9
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-02-23 17:17:32 +0530

    arrays & objects done

---
+++ Week 07/day-1/02_array_object.js
@@ -0,0 +1,54 @@
+let chaiTypes = ["Masala Chai", "Ginger chai", "Green Tea", "Lemon Tea"];
+
+// console.log(chaiTypes[2]);
+// console.log(`Total chai Types: ${chaiTypes.length}`);
+// chaiTypes.push("Herbal Tea");
+// console.log(chaiTypes);
+
+// const data = chaiTypes.pop();
+// console.log(data);
+// console.log(chaiTypes);
+
+// let index = chaiTypes.indexOf("Green Tea");
+// console.log(index);
+// if (index !== -1) {
+//   chaiTypes.splice(index, 1);
+// }
+// console.log(chaiTypes);
+
+// chaiTypes.forEach((chai, index) => {
+//   console.log(`${index + 1}: ${chai}`);
+// });
+
+// let moreChaiTypes = ["Oolong Tea", "White Tea"];
+// let allChaiTypes = chaiTypes.concat(moreChaiTypes);
+// console.log(allChaiTypes);
+
+// let newChaiTypes = [...chaiTypes, "Chamolina Tea"];
+// console.log(newChaiTypes);
+
+// let chaiRecipe = {
+//   name: "Masala Chai",
+//   ingredients: {
+//     teaLeaves: "Assam Tea",
+//     milk: "Full Cream Milk",
+//     sugar: "Brown sugar",
+//     spices: ["Daalchini", "Ginger"],
+//   },
+//   instruction: "Boil water, add tea leaves, milk, sugar and spices",
+// };
+
+// console.log(chaiRecipe.ingredients.spices[1]);
+
+// let updatedChaiRecepie = {
+//   ...chaiRecipe,
+//   instruction: "Boil water, add tea leaves, mil, sugar, spices with some love",
+// };
+
+// console.log(updatedChaiRecepie);
+
+// let { name, ingredients } = chaiRecipe;
+// let [pehliBaarHaiJi, doosriBaarHaiJi, demo] = chaiTypes;
+// console.log(name);
+// console.log(ingredients);
+// console.log(pehliBaarHaiJi, doosriBaarHaiJi, demo);
