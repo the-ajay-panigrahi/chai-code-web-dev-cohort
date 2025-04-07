d141c277ca8ad2f823e74ab685a31c2e60e358f9
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-04-07 13:47:59 +0530

    mega project setup

--- Week 12/src/app.js
+++ Week 12/src/app.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+// importing routes
+import healthCheckRouter from "./routes/healthcheck.route.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 export default app;
---
+++ Week 12/src/controllers/healthcheck.controller.js
@@ -0,0 +1,7 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const healthCheck = (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
+};
+
+export { healthCheck };
